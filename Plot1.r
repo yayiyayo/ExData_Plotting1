b13ac45488ea777e11f303284dca10fa3c535fa7
yayiyayo <lynnlinxing@hotmail.com>
2016-04-11 13:51:47 -0700

    Delete Plot1.r

--- Plot1.r
+++
@@ -1,8 +0,0 @@
-data<-read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
-data$Date <- as.Date(data$Date , format = "%d/%m/%Y")
-subdata <- data[data$Date >= "2007-02-01" & data$Date <= "2007-02-02",]
-
-library(datasets)
-png(file="plot1.png")
-hist(subdata$Global_active_power,col="red",main="Global Active Power", xlab ="Global Active Power (kilowatts)")
-dev.off()
