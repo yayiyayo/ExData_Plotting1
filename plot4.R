7e74372f646e59ad8e93061084cc5f5d8b00b13c
yayiyayo <lynnlinxing@hotmail.com>
2016-04-11 13:50:22 -0700

    Added files via upload

--- plot4.R
+++ plot4.R
@@ -12,9 +12,9 @@ with(subdata,plot(fulltime,Global_active_power,type="l",ylab ="Global Active Pow
 #plot2
 with(subdata,plot(fulltime,Voltage,type="l",xlab="datetime"))
 #plot3
-with(subdata,plot(fulltime,Sub_metering_1, ylim=c(0,30),type="l",ylab ="Energy sub metering",xlab=""))
-with(subdata,points(fulltime,Sub_metering_2, ylim=c(0,20),type="l",col="red"))
-with(subdata,points(fulltime,Sub_metering_3, ylim=c(0,20),type="l",col="blue"))
+with(subdata,plot(fulltime,Sub_metering_1,type="l",ylab ="Energy sub metering",xlab=""))
+with(subdata,points(fulltime,Sub_metering_2,type="l",col="red"))
+with(subdata,points(fulltime,Sub_metering_3,type="l",col="blue"))
 legend("topright",pch="-",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex =0.8)
 #plot4
 with(subdata,plot(fulltime,Global_reactive_power,type="l",xlab="datetime"))
